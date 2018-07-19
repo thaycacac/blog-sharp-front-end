ab79b39ab62c0ee4a06fc167b5db8b5e9d280797
thaycacac <thaycacac@gmail.com>
2018-07-20 02:11:19 +0700

    upload project

---
+++ js/sharpteam.js
@@ -0,0 +1,53 @@
+// Hoa
+var canvas = document.getElementById('canvas-line-team');
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    // Line
+    ctx.fillRect(240, 30, 8, 150);
+    // Circle
+    var circle = canvas.getContext("2d");
+    circle.beginPath();
+    circle.arc(244, 18, 12, 0, 2 * Math.PI);
+    circle.stroke();
+    // Nav
+    ctx.beginPath();
+    ctx.moveTo(280, 20);
+    ctx.lineTo(300, 35);
+    ctx.lineTo(300, 7);
+    ctx.fill();
+}
+var canvas2 = document.getElementById('canvas-line-team-2');
+if (canvas2.getContext) {
+    var ctx2 = canvas2.getContext('2d');
+    // Line
+    ctx2.fillRect(240, 30, 8, 150);
+    // Circle
+    var circle2 = canvas2.getContext("2d");
+    circle2.beginPath();
+    circle2.arc(244, 18, 12, 0, 2 * Math.PI);
+    circle2.stroke();
+    // Nav
+    ctx2.beginPath();
+    ctx2.moveTo(280, 20);
+    ctx2.lineTo(300, 35);
+    ctx2.lineTo(300, 7);
+    ctx2.fill();
+}
+var canvas3 = document.getElementById('canvas-line-team-3');
+if (canvas3.getContext) {
+    var ctx3 = canvas3.getContext('2d');
+    // Line
+    ctx3.fillRect(240, 30, 8, 150);
+    // Circle
+    var circle3 = canvas3.getContext("2d");
+    circle3.beginPath();
+    circle3.arc(244, 18, 12, 0, 2 * Math.PI);
+    circle3.stroke();
+    // Nav
+    ctx3.beginPath();
+    ctx3.moveTo(280, 20);
+    ctx3.lineTo(300, 35);
+    ctx3.lineTo(300, 7);
+    ctx3.fill();
+}
+// Hoa
